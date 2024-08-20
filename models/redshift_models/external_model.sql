17d3d2a118864ac3b5faa313b5962ade3c857b50
ashok@dqlabs.ai <ashok@dqlabs.ai>
2024-08-20 12:52:30 +0000

    redshift spectrum

---
+++ models/redshift_models/external_model.sql
@@ -0,0 +1,14 @@
+WITH people_data AS (
+    SELECT
+        id,
+        first_name,
+        last_name,
+        email
+    FROM
+        {{ source('redshift_external', 'people_dbt_core') }}
+)
+
+SELECT
+*
+FROM
+    people_data
