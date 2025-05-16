29124aaed49c1e1dffb76ded7d91ed1aab4f741e
Nikhil Tomar <nikhil.t@dqlabs.ai>
2025-05-16 13:20:52 +0000

    commit

--- models/Pipeline_Obs/banking_data_vw.sql
+++ models/Pipeline_Obs/banking_data_vw.sql
@@ -5,17 +5,19 @@
 
 SELECT 
     c.customer_id,
-    c.customer_name,
     c.email,
     l.loan_id,
     l.loan_amount,
     l.loan_status,
     t.transaction_id,
     t.transaction_date,
-    t.amount AS transaction_amount
+    t.transaction_amount
 FROM 
     {{ ref('customers') }} c
 LEFT JOIN 
-    {{ ref('loans') }} l ON c.customer_id = l.customer_id
+    {{ ref('loan') }} l ON c.customer_id = l.customer_id
 LEFT JOIN 
     {{ ref('transactions') }} t ON c.customer_id = t.customer_id
+  
+
+
