4eeac28a13eb795b9a41201a987943de9beeec68
srinivas Tambabathula <venkata_srinivas@dqlabs.ai>
2025-07-30 10:22:23 +0000

    commit

--- models/Snowflake_TL_SR/marts/electronics/gold_electronics_sales.sql
+++ models/Snowflake_TL_SR/marts/electronics/gold_electronics_sales.sql
@@ -1,19 +1,20 @@
-{{
-  config(
-    materialized='table',
-    tags=['gold', 'electronics']
-  )
-}}
+{{ config(
+    materialized = 'table',
+    tags = ['gold', 'electronics']
+) }}
+
 WITH fct_sales AS (
     SELECT * 
-    FROM DQLABS_QA.DBT_MODELS.fct_electronics_sales
+    FROM {{ ref('fct_electronics_sales') }}
 )
 
 SELECT 
-    RECORD_ID
-   ,DATE,
+    RECORD_ID,
+    SALE_DATE,
     STORE_ID,
     PRODUCT_NAME,
-    SALE_YEAR
+    SALE_YEAR,
+    CURRENT_TIMESTAMP() AS LOADED_AT,
+    '{{ invocation_id }}' AS DBT_RUN_ID
 FROM fct_sales
 WHERE SALE_YEAR IS NOT NULL
