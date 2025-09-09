3d1b873c73a50a9516542b0ab7ae018157c97445
srinivas Tambabathula <venkata_srinivas@dqlabs.ai>
2025-09-09 05:08:19 +0000

    update sqlfile

--- models/metadatacheck/stg_transactions.sql
+++ models/metadatacheck/stg_transactions.sql
@@ -1,12 +1,5 @@
-{{config(
-        materialized='table'   
-    )
-}}
-
 with stg_claimstx as (
     select * 
     from DQLABS_QA.ZTEST.CLAIM_TX
 )
-
-select *
-from stg_claimstx
+select * from stg_claimstx
