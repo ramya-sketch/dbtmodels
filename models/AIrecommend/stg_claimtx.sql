bfff27320f4b0b9e3e95ffac26211f405caccb10
srinivas Tambabathula <venkata_srinivas@dqlabs.ai>
2025-09-11 08:13:38 +0000

    testfail

--- models/AIrecommend/stg_claimtx.sql
+++ models/AIrecommend/stg_claimtx.sql
@@ -1,5 +1,7 @@
-with stg_claimtx as (select
+{{ config(
+    materialized = 'table'   
+) }}
+select
     CLAIM_ID,
-    CAST(TX_ID AS NUMBER) AS TX_Numb
-from DQLABS_QA.ZTEST.CLAIM_TX)
-select * from stg_claimtx
+    cast(TX_ID as number) as TX_Numb
+from DQLABS_QA.ZTEST.CLAIM_TX
