3c5a6f58343f0c1d5de5e30dfc5481c4d168a960
srinivas qa <venkata_srinivas@dqlabs.ai>
2025-12-17 13:03:16 +0000

    test_new

--- tests/stg_customers_test.sql
+++ tests/stg_customers_test.sql
@@ -1,4 +1,7 @@
 select *
 from {{ ref('stg_customers') }}
-where trim(first_name) = '' or NULL
+where
+      first_name is null
+   or trim(first_name) = ''
+   or last_name is null
    or trim(last_name) = ''
