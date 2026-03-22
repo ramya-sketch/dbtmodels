c6261137cd37a067e1260069677b17b65db3c645
ramya-sketch <ramya@dqlabs.ai>
2026-03-22 22:04:35 +0530

    Update stg_claims.sql

--- models/AIrecommend/stg_claims.sql
+++ models/AIrecommend/stg_claims.sql
@@ -4,11 +4,11 @@
 
 select
     CLAIM_ID,
-    CLAIM_NUMBER,
     POLICY_NUMBER,
     CLAIM_DATE,
     CLAIM_TYPE,
     STATUS,
     accident_date, 
+    created_date
     reported_date
 from DQLABS_QA.ZTEST.CLAIM
